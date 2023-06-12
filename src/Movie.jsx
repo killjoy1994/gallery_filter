1c6413854231a3e8b2dac72d1aa78dfa22a6a647
Bagus Nugroho <papaobobo@gmail.com>
2023-06-13 02:00:03 +0700

    fix bug card title length

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
+import { twMerge } from 'tailwind-merge'
 
 export default function Movie({movie}) {
-  let fontSize
   return (
-    <div className='flex flex-col h-full justify-end'>
-        <h1 className='text-xl font-medium pb-2'>{movie.title}</h1>
-        <img className='rounded-md md:h-[350px] object-fill' src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} alt="" />
+    <div className='flex flex-col h-[300px]'>
+        <h1 className={twMerge("font-medium pb-2", movie.title.length > 25 ? "text-md " : "text-xl")}>{movie.title}</h1>
+        <img className='rounded-md object-fill h-full hover:opacity-75 shadow-md' src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} alt="" />
     </div>
   )
 }
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 export default function () {
+  
   return (
-    <div>
-      <button>All</button>
-      <button>Comedy</button>
-      <button>Action</button>
+    <div className="flex gap-x-4 mx-auto justify-center">
+      <button className="inline-block text-3xl font-semibold hover:opacity-70">All</button>
+      <button className="inline-block text-3xl font-semibold hover:opacity-70">Comedy</button>
+      <button className="inline-block text-3xl font-semibold hover:opacity-70">Action</button>
     </div>
   );
 }
