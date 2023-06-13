185bbeb3cd95b5a5e05a20fa8b595a37a46d4865
Bagus Nugroho <papaobobo@gmail.com>
2023-06-14 01:34:55 +0700

    update token in .env

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     headers: {
       accept: "application/json",
       Authorization:
-        "Bearer " + "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTIyYzg3ZDYyZDI4ODUxN2E0NTZiNTNkYzg1OGU3YSIsInN1YiI6IjY0ODYzMTJhOTkyNTljMDBmZjBmNDEwOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oLa38gcYrg_U1IHxnS32SBtBCyx4IMKkVC7bLtor9xU",
+        "Bearer " + import.meta.env.VITE_TOKEN,
     },
   };
 
